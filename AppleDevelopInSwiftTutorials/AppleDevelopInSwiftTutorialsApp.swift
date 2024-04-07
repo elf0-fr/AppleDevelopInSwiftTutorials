61fb5240706ff6168b9abda03cf3e274573f5d7e
Romain Tirbisch <romain_tirbisch@yahoo.com>
2024-04-07 19:32:12 +0200

    Initial Commit

---
+++ AppleDevelopInSwiftTutorials/AppleDevelopInSwiftTutorialsApp.swift
@@ -0,0 +1,32 @@
+//
+//  AppleDevelopInSwiftTutorialsApp.swift
+//  AppleDevelopInSwiftTutorials
+//
+//  Created by Elfo on 07/04/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AppleDevelopInSwiftTutorialsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ AppleDevelopInSwiftTutorials/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AppleDevelopInSwiftTutorials
+//
+//  Created by Elfo on 07/04/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
